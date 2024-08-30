31ab7f2ac5d1778afd35f0eb3d111989d2a592f6
chiragm-evrig <chirag.m@evrig.com>
2024-08-30 16:50:28 +0530

    theme commit

---
+++ beauty/web/tailwind/tailwind.browser-jit-config.js
@@ -0,0 +1,51 @@
+const { spacing } = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+    theme: {
+        container: {
+            center: true
+        },
+        extend: {
+            colors: {
+                'pearl': '#ECDEC1',
+                'floralwhite': '#FCF7EE',
+                'copper': '#B6713E',
+                gray: {
+                    'erieb': '#222222',
+                    'dim': '#666666',
+                }
+            },
+            spacing: {
+                '3.5': '14px',
+                '3.75': '15px',
+                '4.5': '18px',
+                '12.5': '50px',
+                '32.5': '130px',
+                '42.5': '170px'
+            },
+            width: {
+                '180pxc' : 'calc(100% - 180px)',
+                '35%': '35%',
+                '30%': '30%',
+            },
+            maxWidth: {
+                '1536': '1536px',
+            },
+            content: {
+                'blank': '""',
+            },
+            fontSize: {
+              'f0': '0px',
+              'f32': '32px',
+              'f40': '40px'
+            },
+            boxShadow: {
+                '4xl': '1px 1px 4px #ECDEC1',
+            },
+            backgroundImage: {
+                'caret-down' : "url('../images/caret-down.svg')",
+            },
+        },
+    }
+}
---
+++ beauty/web/tailwind/tailwind.config.js
@@ -0,0 +1,158 @@
+const {
+  spacing
+} = require('tailwindcss/defaultTheme');
+
+const colors = require('tailwindcss/colors');
+
+const hyvaModules = require('@hyva-themes/hyva-modules');
+
+module.exports = hyvaModules.mergeTailwindConfig({
+  theme: {
+    extend: {
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+        '2xl': '1536px' // => @media (min-width: 1536px) { ... }
+
+      },
+      fontFamily: {
+        sans: ["Segoe UI", "Helvetica Neue", "Arial", "sans-serif"]
+      },
+      colors: {
+        primary: {
+          lighter: colors.blue['300'],
+          "DEFAULT": colors.blue['800'],
+          darker: colors.blue['900']
+        },
+        secondary: {
+          lighter: colors.blue['100'],
+          "DEFAULT": colors.blue['200'],
+          darker: colors.blue['300']
+        },
+        background: {
+          lighter: colors.blue['100'],
+          "DEFAULT": colors.blue['200'],
+          darker: colors.blue['300']
+        },
+        green: colors.emerald,
+        yellow: colors.amber,
+        purple: colors.violet
+      },
+      textColor: {
+        orange: colors.orange,
+        red: { ...colors.red,
+          "DEFAULT": colors.red['500']
+        },
+        primary: {
+          lighter: '#222222',
+          "DEFAULT": '#222222',
+          darker: '#222222',
+        },
+        secondary: {
+          lighter: '#666666',
+          "DEFAULT": '#666666',
+          darker: '#666666'
+        }
+      },
+      backgroundColor: {
+        primary: {
+          lighter: colors.blue['600'],
+          "DEFAULT": colors.blue['700'],
+          darker: colors.blue['800']
+        },
+        secondary: {
+          lighter: colors.blue['100'],
+          "DEFAULT": colors.blue['200'],
+          darker: colors.blue['300']
+        },
+        container: {
+          lighter: '#ffffff',
+          "DEFAULT": '#fafafa',
+          darker: '#f5f5f5'
+        }
+      },
+      borderColor: {
+        primary: {
+          lighter: colors.blue['600'],
+          "DEFAULT": colors.blue['700'],
+          darker: colors.blue['800']
+        },
+        secondary: {
+          lighter: colors.blue['100'],
+          "DEFAULT": colors.blue['200'],
+          darker: colors.blue['300']
+        },
+        container: {
+          lighter: '#f5f5f5',
+          "DEFAULT": '#e7e7e7',
+          darker: '#b6b6b6'
+        }
+      },
+      minWidth: {
+        8: spacing["8"],
+        20: spacing["20"],
+        40: spacing["40"],
+        48: spacing["48"]
+      },
+      minHeight: {
+        14: spacing["14"],
+        a11y: '44px',
+        'screen-25': '25vh',
+        'screen-50': '50vh',
+        'screen-75': '75vh'
+      },
+      maxHeight: {
+        '0': '0',
+        'screen-25': '25vh',
+        'screen-50': '50vh',
+        'screen-75': '75vh'
+      },
+      container: {
+        center: true,
+        padding: '1.5rem'
+      }
+    }
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+  // Examples for excluding patterns from purge
+  content: [
+    // this theme's phtml and layout XML files
+    '../../**/*.phtml',
+    '../../*/layout/*.xml',
+    '../../*/page_layout/override/base/*.xml',
+    // parent theme in Vendor (if this is a child-theme)
+    '../../../../../../../vendor/hyva-themes/magento2-default-theme/**/*.phtml',
+    '../../../../../../../vendor/hyva-themes/magento2-default-theme/*/layout/*.xml',
+    '../../../../../../../vendor/hyva-themes/magento2-default-theme/*/page_layout/override/base/*.xml',
+    // app/code phtml files (if need tailwind classes from app/code modules)
+    //'../../../../../../../app/code/**/*.phtml',
+  ]
+});
+if (require('fs').existsSync('./tailwind.browser-jit-config.js')) {
+  function isObject(item) {
+    return (item && typeof item === 'object' && !Array.isArray(item));
+  }
+  function mergeDeep(target, ...sources) {
+    if (!sources.length) return target;
+    const source = sources.shift();
+
+    if (isObject(target) && isObject(source)) {
+      for (const key in source) {
+        if (isObject(source[key])) {
+          if (!target[key]) Object.assign(target, { [key]: {} });
+          mergeDeep(target[key], source[key]);
+        } else {
+          Object.assign(target, { [key]: source[key] });
+        }
+      }
+    }
+    return mergeDeep(target, ...sources);
+  }
+  mergeDeep(module.exports, require('./tailwind.browser-jit-config.js'));
+}
